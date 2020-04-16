293ea771e16ea0463ace9daf8c69c1c9f1e44cb1
Paula Urteaga <paulaurteaga@Paulas-MacBook-Pro.local>
2020-04-15 22:42:57 -0700

    added

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZG9jY290ZWFtIiwiYSI6ImNrOGY4YmFtaDAxbnAzbnMwZW1hemEwYXgifQ.7jwbBw9xXZTDhQ41OzLdYw";
---
+++ script.js
@@ -0,0 +1,117 @@
+url="https://opendata.arcgis.com/datasets/4a702cd67be24ae7ab8173423a768e1b_0.geojson"
+
+
+
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: API_KEY
+});
+var myMap = L.map("map", {
+    center: [34.052235, -118.243683],
+    zoom: 7,
+  });
+  // Adding tile layer to the map
+  lightmap.addTo(myMap);
+
+var gData
+var onlineyear
+var energy_type
+var MWproduction
+var stationID
+var income
+var county
+var state
+function init(){
+  var select = d3.select("#selectNumber");
+  d3.json(url,(data) => {
+    gData=data
+    var sampleNames=data.features
+     sampleNames.forEach((sample) => {
+        select
+        .append("option")
+        .text(sample.properties.General_Fuel)
+        .property("value", sample.properties.General_Fuel);  
+    });
+    L.geoJson(data, {
+        onEachFeature: function(feature, layer) {
+          var newMarker = L.marker([feature.properties.Latitude, feature.properties.Longitude]);
+          newMarker.addTo(myMap);
+          newMarker.bindPopup("Plant Label: " + feature.properties.Plant_Label + "<br>");
+        }
+      })
+   
+  }
+  )
+  initBarchart()
+  initBubbleChart()
+  initTimeline()
+  initPieChart()
+}
+console.log(gData)
+function newId(sample){
+    var sampleRows=gData.features
+    var filterData=sampleRows.filter(d=>d.properties.General_Fuel==sample)
+    onlineyear=filterData.map(d=>d.properties.Online_Year==null?1990:d.properties.Online_Year)
+    energy_type=filterData.map(d=>d.properties.General_Fuel)
+    MWproduction=filterData.map(d=>d.properties.MW)
+    stationID=filterData.map(d=>d.properties.Plant_ID)
+    income=filterData.map(d=>d.properties.Net_MWh==null?50:d.properties.Net_MWh)
+    county=filterData.map(d=>d.properties.County)
+    state=filterData.map(d=>d.properties.state)
+    
+    
+    //createBarChart( MWproduction, state, county);
+    createBarChart(energy_type,MWproduction)
+    createBubblechart(onlineyear,MWproduction,energy_type, filterData)
+    createTimeline(energy_type,income)
+    createPieChart()
+   
+   }
+
+
+
+  function createBarChart(energy_type, MWproduction){
+    var trace = {
+      x: energy_type,
+      y: MWproduction,
+      type: "bar"
+    };
+    
+    var data = [trace];
+    var layout = {
+      title: "Sources of Energy in California",
+      xaxis: { title: "Type of Energy" },
+      yaxis: { title: "Total Capacity in MWH"}
+    };
+    Plotly.newPlot("bar", data, layout);
+  }
+function createBubblechart(onlineyear, MWproduction, energy_type, filterData) {
+   
+}
+
+function createTimeline(energy_type,income,MWproduction){
+   
+
+}
+function createPieChart(){
+
+}
+
+
+function initBarchart(){
+
+}
+function initBubbleChart(){
+
+}
+function initTimeline(){
+
+}
+function initPieChart(){
+
+}
+
+
+init()
